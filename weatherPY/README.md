3f81554c5d416a43bb053ededaadaf86c7acc2bf
theyoungerelder <jelder@uoregon.edu>
2023-09-09 14:07:08 -0700

    finsihed sorta -je

--- weatherPY/README.md
+++ weatherPY/README.md
@@ -1,2 +1,2 @@
 # python-api-challenge
-challenge 6 for data class 
+challenge 6 for data class. in this challenge I 1. visualized info about weather over 500 cities that are difff distances from the equator. 
---
+++ weatherPY/api_keys.py
@@ -0,0 +1,15 @@
+
+# openweath api
+weather_api_key = 'c0af7e1bcf7bc6082823a61256f62e72'
+
+# geoappify key
+geoapify_key = "db6086669ffd45e4b1714dabe56f2cbe"
+
+
+
+
+print(weather_api_key)
+
+print(geoapify_key)
+
+
